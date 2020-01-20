04cf7ea7e57c22f26c5914d26f1eeb1089d20590
Ihor Stavratii <igor.stavratiy@gmail.com>
2020-01-20 11:04:46 +0200

    Updates

---
+++ precache-manifest.28b30d63ebfc84f107912d97c720a9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9a28599e4c49548f3706eadfe35705",
+    "url": "/userlist-firebase/index.html"
+  },
+  {
+    "revision": "16f650838a8d624ce908",
+    "url": "/userlist-firebase/static/css/main.98aedec3.chunk.css"
+  },
+  {
+    "revision": "80252eb26ef87f24904e",
+    "url": "/userlist-firebase/static/js/2.5beb3dcd.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/userlist-firebase/static/js/2.5beb3dcd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16f650838a8d624ce908",
+    "url": "/userlist-firebase/static/js/main.79eac580.chunk.js"
+  },
+  {
+    "revision": "d8cf31e8503318c1e15f",
+    "url": "/userlist-firebase/static/js/runtime-main.06fe364b.js"
+  }
+]);
